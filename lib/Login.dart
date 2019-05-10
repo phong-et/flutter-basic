a152ef309d7dd5b69436ebee68f56a1029cd3b57
Phillip <phillip7.et@gmail.com>
2019-05-10 23:21:54 +0700

    create login page, add firebase

---
+++ lib/Login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    final usernameField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Username",
+          prefixIcon: Icon(Icons.account_box),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          prefixIcon: Icon(Icons.lock),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffF8716B),
+      child: Container(
+        child: FlatButton.icon(onPressed: null, icon: Icon(Icons.blur_on), label: Text('SignIn'))
+      )
+    );
+
+    return Scaffold(
+      // backgroundColor: Color(0xffF4D0CC),
+      body: Center(
+        child: Container(
+          color: Color(0xffF4D0CC),
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: ListView(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    height: 155.0,
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      fit: BoxFit.contain,
+                    )),
+                SizedBox(height: 45.0),
+                usernameField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(height: 35.0),
+                loginButon,
+                SizedBox(height: 15.0)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
